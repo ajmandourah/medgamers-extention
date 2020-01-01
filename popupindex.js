705a0989c4977005f993c3ce2ba1a3457f68777a
ajmandourah <ajmandourah@gmail.com>
2020-01-01 19:02:53 +0100

    initial

---
+++ popupindex.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var mr = new mumbleReader(
+  'http://medgamers.duckdns.org:21803/1',
+  'mum0'
+) //init the object
+mr.settooltip(false) //to disable the tooltip change true to false
+mr.setuseservername(true) //Use the server name as name of the root channel
+mr.setlenght(50) //Set the max lenght for displaying or remove this line to have no limit
+mr.setimgpath('icons/') //Set the image path the local subdirectory MAKE SURE THAT U UPLOAD THE ICONS
+mr.setuseservername(true) //Do not show root in the Root channel but the given servername
+function load_mum() {
+  mr.start() //This will reload the viewer
+}
+load_mum() //This will to the loading the first time
+window.setInterval(() => {
+  load_mum()
+}, 50000)
+  })
